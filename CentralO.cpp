034984707e9d28bd9af316aa6b1b0486ed473394
Gabriel <gabrielgonzalezlisboa@gamil.com>
2025-09-10 16:09:21 -0300

    ajustes a read, cambio de path

--- CentralO.cpp
+++ CentralO.cpp
@@ -2,19 +2,21 @@
 #include <unistd.h>   // pipe(), fork(), read(), write()
 #include <cstring>    // strlen()
 #include <sys/stat.h> // mkfifo()
-#include <fcntl.h>    // open() y flags O_READ, O_WRONLY
+#include <fcntl.h>    // open() y flags O_RDONLY, O_WRONLY
 
 // Proceso central o tambien 'Orquestador' de la comunicacion entre procesos
 
 int main() {
-    const char* C2O = "/IPC/pipe";
+    const char* C2O = "/tmp/orch_c2o.fifo"; // cliente -> orquestador
     mkfifo(C2O, 0666); // creacion de la named pipe
-    return 0;
-
+    std::cout << "orquestador iniciado, esperando mensajes del cliente" << std::endl;
     int clientToOrch = open(C2O, O_RDONLY ); // pipe del cliente al orquestador
     char buffer[100];
-    read(clientToOrch, buffer, sizeof(buffer)); // lee el mensaje del cliente
-    std::cout << "Mensaje recibido del cliente: " << buffer << std::endl;
+    ssize_t n = read(clientToOrch, buffer, sizeof(buffer)); // lee el mensaje del cliente, size n para que sea seguro
+    if (n > 0) {
+        buffer[n] = '\0'; // el buffer es una cadena
+        std::cout << "Mensaje recibido del cliente: " << buffer << std::endl;
+    }
 
     close(clientToOrch);
     unlink(C2O); // elimina la named pipe
