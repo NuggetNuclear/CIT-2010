553fe887d5eda70eca32f4ffbaecfb9793662fb4
Gabriel <gabrielgonzalezlisboa@gamil.com>
2025-09-10 15:40:06 -0300

    Orchestrator

---
+++ CentralO.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <unistd.h>   // pipe(), fork(), read(), write()
+#include <cstring>    // strlen()
+
+// Proceso central o tambien 'Orquestador' de la comunicacion entre procesos
+
+int main() {
+    return 0;
+}
