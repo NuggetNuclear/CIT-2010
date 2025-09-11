331f8458ed1769d030075df984085029b959a13d
Gabriel <gabrielgonzalezlisboa@gamil.com>
2025-09-10 23:18:23 -0300

    .

--- central.cpp
+++ central.cpp
@@ -15,37 +15,9 @@ int main() {
     mkfifo(C2O, 0666);
     mkfifo(O2C, 0666);
 
-    std::cout << "[CENTRAL] Iniciado - Esperando mensaje..." << std::endl;
+    std::cout << "[CENTRAL] Bienvenido al Chat Comunitario" << std::endl;
 
     int clientToOrch = open(C2O, O_RDONLY); // Cliente --> Orquestador
     int orchToClient = open(O2C, O_WRONLY); // Orquestador --> Cliente
-
-    char buffer[1024];
-    while (true) {
-        // Limpieza de buffer
-        memset(buffer, 0, sizeof(buffer));
-        
-        // Leer mensaje del cliente
-        ssize_t bytesRead = read(clientToOrch, buffer, sizeof(buffer) - 1);
-        if (bytesRead > 0) {
-            buffer[bytesRead] = '\0';
-            std::cout << "[CENTRAL] Mensaje recibido: " << buffer << std::endl;
-            std::string response = "Orquestador recibió: " + std::string(buffer);
-            write(orchToClient, response.c_str(), response.size());
-        } else if (bytesRead == 0) { // Cliente desconectado
-            std::cout << "[CENTRAL] Cliente desconectado." << std::endl;
-            break;
-        } else { // Error de lectura
-            std::cerr << "[CENTRAL] Error al leer del pipe." << std::endl;
-            break;
-        }
-    }
-
-    close(clientToOrch);
-    close(orchToClient);
-
-    unlink(C2O);
-    unlink(O2C);
-
-    return 0;
+ 
 }
--- cliente.cpp
+++ cliente.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-#include <unistd.h>   // pipe(), fork(), read(), write()
-#include <cstring>    // strlen()
-#include <sys/stat.h> // mkfifo()
-#include <fcntl.h>    // open() y flags O_RDONLY, O_WRONLY
-
-// Proceso cliente que envia mensajes al orquestador
+#include <string>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <cstring>
 
 int main() {
     const char* C2O = "/tmp/orch_c2o.fifo"; // cliente -> orquestador
@@ -13,24 +12,10 @@ int main() {
     mkfifo(C2O, 0666);
     mkfifo(O2C, 0666);
 
-    std::cout << "[CLIENTE] Conectando...\n";
-
     int wfd = open(C2O, O_WRONLY);
     int rfd = open(O2C, O_RDONLY);
 
-    const char* msg = "fokin pipe\n";
-    (void)write(wfd, msg, std::strlen(msg)); // simple write
-
-    char buf[1024];
-    ssize_t n = read(rfd, buf, sizeof(buf) - 1);
-    if (n > 0) {
-        buf[n] = '\0';
-        std::cout << "[CLIENTE] respuesta: " << buf;
-    } else if (n == 0) {
-        std::cout << "[CLIENTE] central cerró la conexión.\n";
-    } else {
-        std::cerr << "[CLIENTE] error de lectura.\n";
-    }
+    pid_t clienteID = getpid();
 
     close(wfd);
     close(rfd);
