e6a35ba6ba6070e562dbd47f7d36f26ca173b7ef
Matias Vigneau <164396684+vigneauuu@users.noreply.github.com>
2025-11-02 23:29:09 -0300

    terea2 finalll

--- T2/src/config.h
+++ T2/src/config.h
@@ -3,11 +3,9 @@
 
 #include <pthread.h>
 
-#define MAX_PATH 100
-#define MAX_MONSTERS 20
-#define MAX_HEROES 5   // Soporte para N héroes
-
-// ----------------- Estructuras -----------------
+#define MAX_PATH 200
+#define MAX_MONSTERS 30
+#define MAX_HEROES 5
 
 typedef struct {
     int x;
@@ -15,7 +13,7 @@ typedef struct {
 } Point;
 
 typedef struct {
-    int id; 
+    int id;
     int hp;
     int attack;
     int range;
@@ -23,12 +21,10 @@ typedef struct {
     Point path[MAX_PATH];
     int path_len;
     Point posActual;
-
-    int en_combate; 
+    int en_combate;
     int path_step;
     int path_finished;
-    int escapado;      // 1 si llegó al final y salió del mapa
-
+    int escapado;
 } Hero;
 
 typedef struct {
@@ -38,29 +34,21 @@ typedef struct {
     int vision;
     int range;
     Point pos;
-    int alertado;        // 0 = dormido, 1 = alerta
-    int target_hero_id;  // ID del heroe que persigue (-1 = ninguno)
+    int alertado;
+    int target_hero_id;
 } Monster;
 
 typedef struct {
     int width;
     int height;
-
     Hero heroes[MAX_HEROES];
     int hero_count;
-
     Monster monsters[MAX_MONSTERS];
     int monster_count;
-
-    // Sincronización y estado global
-    pthread_mutex_t mutex; 
+    pthread_mutex_t mutex;
     int juegoActivo;
-
 } GameConfig;
 
-// Lee un archivo de configuración. Acepta:
-//   (A) Formato base del enunciado (1 héroe): HERO_HP, HERO_START, HERO_PATH, MONSTER_1_*, ...
-//   (B) Formato extendido (N héroes): HERO <id> CAMPO..., MONSTER <id> CAMPO...
 int leerConfig(const char *nombreArchivo, GameConfig *cfg);
 
 #endif
